50ccee66bef8da7ad0239066570c38465b0cd439
Noel Niles <shakabra321@gmail.com>
2015-05-18 18:03:03 -1000

    Begin adding javascript
    This is just a basic template and will probably change a lot.

--- wordplay.js
+++ wordplay.js
@@ -1,15 +1,56 @@
 window.onload = function(){
-    wordplay.init();
+    game.init();
 }
 
-wordplay = {
+game = {
+    id: '',
 
-    init:
-    function(){},
+    init: function(){},
 
-    put_tile:
-    function(){},
+    scores = {
+        p1: '',
+        p2: '',
+        p3: '',
+        p4: '',
+    },
+    
+    put_tile: function(){},
 
-    rm_tile:
-    function(){},
+    rm_tile: function(){},
+}
+
+tile = {
+    ch: '',
+    val: '',
+    freq: '',
+}
+
+bag = {
+    letters: [],
+    num_remaining: function(){
+        return this.all_letters.length;
+    }
+    empty: '',
+}
+
+rack = {
+    letters: [],
+    shuffle: function(){
+        // shuffles the letters in the rack so the player can think of new words  
+        return this.letters.sort(function(){return 0.5 - Math.random()});
+    },
+    get_letters: function(){
+        return this.letters
+    },
+    slide_tile: function(){
+        // lets the player drag tiles around the rack
+    },
+}
+
+player = {
+    id: '',
+    name: '',
+    word_list: [],
+    score: '',
+    rack: '',
 }
